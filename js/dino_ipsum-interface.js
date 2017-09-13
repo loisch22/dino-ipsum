8a4e3d26776896fffc7d5e81fb999b08df3c0e97
Lois Choi <loisch22@gmail.com>
2017-09-13 13:11:15 -0700

    add code using promises and dino-ipsum api

--- js/dino_ipsum-interface.js
+++ js/dino_ipsum-interface.js
@@ -6,14 +6,26 @@ $(function() {
     $('#paragraphs').val("");
     $('#words').val("");
 
-    let getDinos = $.get(`http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=${paragraphs}&words=${words}`),
-    fillContainer = function(html) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://dinoipsum.herokuapp.com/api/?format=html&words=${words}&paragraphs=${paragraphs}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+          //when resolved - recieves value and ready to move on to then function to anyone referencing promise
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(html) {
       $('.showDinoIpsum').html(html);
-    },
-    oops = function() {
-      console.log('Where did all the dinosaurs go?');
-    };
-    getDinos.then(fillContainer, oops);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}.`);
+    });
   });
   $('#reset-btn').click(function() {
     let paragraphs = $('#paragraphs').val();
@@ -22,77 +34,4 @@ $(function() {
     $('.showDinoIpsum').html("");
   });
 });
-
-
-
-
-
-//     let promise = new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9204738acaf5b47d8cef3c840b1ffe27`;
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         } else {
-//           reject(Error(reqest.statusText));
-//         }
-//         request.open("GET", url, true);
-//         request.send();
-//       }
-//     });
-//
-//     promise.then(function(response) {
-//       body = JSON.parse(response);
-//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-//     }, function(error) {
-//       $('.showErrors').text(`There was an error processing your request: ${error.message}.`);
-//     });
-//   });
-// });
-
-
-
-
-
-
-
-//     $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9204738acaf5b47d8cef3c840b1ffe27`).then(function(response) {
-//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-//     }).fail(function() {
-//       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`)
-//     });
-//   });
-// });
-
-
-
-
-
-
-
-
-
-
-// var apiKey = require('./../.env').apiKey;
-//
-// $(function() {
-//   $('#weatherLocation').click(function() {
-//     let city = $('#location').val();
-//     $('#location').val("");
-//     $.ajax({
-//       url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9204738acaf5b47d8cef3c840b1ffe27`,
-//         type: 'GET',
-//         data: {
-//           format: 'json'
-//         },
-//         success: function(response) {
-//           $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-//           $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-//         },
-//         error: function() {
-//           $('#errors').text("There was an error processing your request. Please try again.")
-//         }
-//     });
-//   });
+  
